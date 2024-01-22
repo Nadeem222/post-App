c7b81543835f9e4aa6ac83d1f10a56e1b8e18357
Nadeem Hakeem <110339895+Nadeem222@users.noreply.github.com>
2024-01-22 18:15:02 +0500

    some cahnges

--- app.js
+++ app.js
@@ -14,53 +14,66 @@ let arr = Array.from(toggles);
 
 
 
-arr.forEach(toggles => {
-    toggles.addEventListener('click', (e) => {
+// arr.forEach(toggles => {
+//     toggles.addEventListener('click', (e) => {
         
-        // console.log(e.target.classList[0])
+//         console.log(e.target.src)
 
-        let inputBackkground =  e.target.classList[0];
+//         let inputBackkground =  e.target.classList[0];
 
-        textarea.style.background = '';
-        textarea.style.backgroundImage = '';
-        textarea.style.backgroundColor = '';
-        centre.style.height = '';
+//         textarea.style.background = '';
+//         textarea.style.backgroundImage = '';
+//         textarea.style.backgroundColor = '';
+//         centre.style.height = '';
 
-        if(inputBackkground === "optOne"){
-            textarea.style.background = "#fff" ;
-            centre.style.height = "100%";
+//         if(inputBackkground === "optOne"){
+//             textarea.style.background = "#fff" ;
+//             centre.style.height = "100%";
 
-        }else if(inputBackkground === "optTwo" ){
-            textarea.style.backgroundImage = "url(./img/optTwo.webp)" ;
-            centre.style.height = "100%"
+//         }else if(inputBackkground === "optTwo" ){
+//             textarea.style.backgroundImage = "url(./img/optTwo.webp)" ;
+//             centre.style.height = "100%"
              
-        }else if(inputBackkground === "optThree"){
-            textarea.style.backgroundImage = "url(./img/optThree.webp)"
-            centre.style.height = "100%"
-        }else if(inputBackkground === "optFour"){
-            textarea.style.backgroundImage = "url(./img/optFour.webp)"
-            centre.style.height = "100%"
-        }else if(inputBackkground === "optFive"){
-            textarea.style.backgroundImage = "url(./img/optFive.webp)";
-            centre.style.height = "100%"
-        }else if(inputBackkground === "optSix"){
-            textarea.style.backgroundImage = "url(./img/optSix.webp)";
-            centre.style.height = "100%"
-        }
-        else if(inputBackkground === "optSeven"){
-            textarea.style.backgroundColor = "#C600FF";
-            centre.style.height = "100%"
-        }else if(inputBackkground === "optEight"){
-            textarea.style.backgroundColor = "#E2013B";
-            centre.style.height = "100%"
-        }else if(inputBackkground === "optNine"){
-            textarea.style.backgroundColor = "#000";
-            centre.style.height = "100%"
-        }
+//         }else if(inputBackkground === "optThree"){
+//             textarea.style.backgroundImage = "url(./img/optThree.webp)"
+//             centre.style.height = "100%"
+//         }else if(inputBackkground === "optFour"){
+//             textarea.style.backgroundImage = "url(./img/optFour.webp)"
+//             centre.style.height = "100%"
+//         }else if(inputBackkground === "optFive"){
+//             textarea.style.backgroundImage = "url(./img/optFive.webp)";
+//             centre.style.height = "100%"
+//         }else if(inputBackkground === "optSix"){
+//             textarea.style.backgroundImage = "url(./img/optSix.webp)";
+//             centre.style.height = "100%"
+//         }
+//         else if(inputBackkground === "optSeven"){
+//             textarea.style.backgroundColor = "#C600FF";
+//             centre.style.height = "100%"
+//         }else if(inputBackkground === "optEight"){
+//             textarea.style.backgroundColor = "#E2013B";
+//             centre.style.height = "100%"
+//         }else if(inputBackkground === "optNine"){
+//             textarea.style.backgroundColor = "#000";
+//             centre.style.height = "100%"
+//         }
         
         
-    })
-})
+//     })
+// })
+
+function changeBackground(){
+    console.log(event.target.src)
+    let imageSrc = event.target.src;
+
+    textarea.style.backgroundImage = "url(" + imageSrc + ")";
+
+}
+function textColorChange(){
+    console.log(event.target.value)
+    textarea.style.color = event.target.value
+
+}
 
 post.addEventListener('click' , () => {
     let inputText = textarea.value;
